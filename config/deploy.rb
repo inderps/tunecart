430e27231eaf49f500b4cdc109cb957189a0ea1e
Inderpal Singh <inderpalonline@gmail.com>
2015-07-27 15:34:51 +0530

    aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:inderps/tunescart.git'
 set :deploy_to, '/home/deploy/tunescart'
 
 set :linked_files, %w{config/database.yml config/s3.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 
 # Default branch is :master
